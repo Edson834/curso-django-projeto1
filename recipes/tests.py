c4a258f75e5db4575df56242445ab6ca12192b2f
Edson834 <gelsonandre279@gmail.com>
2025-02-03 12:45:52 +0000

    new

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class RecipeURlsTest(TestCase):
+    def test_the_py_test_is_ok(self):
+        variável='1234'
+        print('hello')
+        assert 1 == 1
