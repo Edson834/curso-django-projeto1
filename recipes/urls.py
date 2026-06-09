0162fe3bfce68a3f6518d2b5958c22207913142d
Edson André <gelsonandre279@gmail.com>
2026-06-09 06:22:31 +0000

    started a new app "recipes", configured its urls and templates. Extra: also added a global folder for base templates

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from recipes.views import home, contato, sobre
+
+    
+
+urlpatterns = [
+    path('', home), # HOME
+    path('contato/', contato), # CONTATO
+    path('sobre/', sobre), # SOBRE
+]
