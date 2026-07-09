a095575118dd13c6a03dc62cb4f47e978bd4f065
Edson André <gelsonandre279@gmail.com>
2026-07-09 08:34:44 +0000

    removed factory.py from utils and fixed some tests

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -14,11 +14,19 @@ class RecipeDetailViewTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
     
     def test_recipes_search_raises_404_if_no_search_term(self):
-        url = reverse('recipes:search') + '?q=Teste'
+        url = reverse('recipes:search')
         response = self.client.get(url)
-        self.assertIn(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
     
     def test_recipe_search_term_is_on_page_title_and_escaped(self):
         url = reverse('recipes:search') + '?q=<Teste>'
         response = self.client.get(url)
         self.assertIn('Search for &quot;&lt;Teste&gt;&quot;', response.content.decode('utf-8'))
+    
+    def test_recipe_search_can_find_recipe_by_title(self):
+        recipe = self.make_recipe(title='Testing Title', description='test desc')
+        response1 = self.client.get(reverse('recipes:search') + '?q=title')
+        response2 = self.client.get(reverse('recipes:search') + '?q=toastle')
+
+        self.assertIn(recipe, response1.context['recipes'])
+        self.assertNotIn(recipe, response2.context['recipes'])
