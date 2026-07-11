4f405a2f1be0cdb27d08c630544bbd8081169580
Edson André <gelsonandre279@gmail.com>
2026-07-11 09:57:19 +0000

    finished the pagination tests and applied pagination to  home template

--- recipes/views.py
+++ recipes/views.py
@@ -2,20 +2,41 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from .models import Recipe
 from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
+
+
 
 def home(request):
     recipes = Recipe.objects.order_by('-id').filter(is_published=True)
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+        
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+    pagination_range = make_pagination_range(paginator.page_range, 4, current_page)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes' : recipes,
+        'recipes' : page_obj,
+        'pagination_range': pagination_range,
+
     })
+
+
 
 def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True))
+    
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes' : recipes,
         'page_title': f'{recipes[0].category.name} - Category | ',
     })
+
+
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
@@ -24,6 +45,8 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
 
 def search(request):
     search_term = request.GET.get('q', '').strip()
