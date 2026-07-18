a44fa1b6cf7c0addc5c07ab0293dcdbc2fcb205e
Edson André <gelsonandre279@gmail.com>
2026-07-18 11:10:20 +0000

    prepared the project for upcoming messages

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
+import os
+
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.pagination import make_pagination
 
 from recipes.models import Recipe
-import os
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
 
 def home(request):
     recipes = Recipe.objects.filter(
