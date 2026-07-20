c48302235c29368117a87fc6678de36e6c3edd02
Edson André <gelsonandre279@gmail.com>
2026-07-20 08:41:17 +0100

    started the new app: authors

--- authors/urls.py
+++ authors/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
     
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
-    path('authors/', include('authors.urls'))
+    path('register/', views.register_view),
 ]
